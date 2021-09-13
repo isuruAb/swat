0d53c642d819758786c6a790b8c5d4c922ea04ed
isuruAb <vpisabeywardana@gmail.com>
2021-09-14 00:11:23 +0800

    navigation works

--- src/components/Navigator/index.js
+++ src/components/Navigator/index.js
@@ -1,16 +1,17 @@
 import { Button } from "react-bootstrap";
+import styles from "./index.module.scss";
 
 const Navigator = ({ onClick, pageNo }) => {
   return (
-    <>
+    <div className={styles.navigationWrapper}>
       <Button variant="primary" onClick={() => onClick("decrease")}>
         {"<"}
       </Button>
-      {pageNo}
+      <span>{pageNo}</span>
       <Button variant="primary" onClick={() => onClick("increase")}>
         {">"}
       </Button>
-    </>
+    </div>
   );
 };
 
