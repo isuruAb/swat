2b021dc94ca43dea61df4b6c26bc5a7e22259fa0
isuruAb <vpisabeywardana@gmail.com>
2021-09-20 12:01:01 +0800

    use URLSearchParams

--- src/utils/api/url.js
+++ src/utils/api/url.js
@@ -4,15 +4,6 @@
  * @returns // string made by concatenated params
  */
 export const getUrlParams = (params) => {
-  let url = "?";
-  const paramsArray = Object.keys(params);
-  paramsArray.map((key, index) => {
-    return (url =
-      url +
-      key +
-      "=" +
-      encodeURIComponent(params[key]) +
-      (index === paramsArray.length - 1 ? "" : "&"));
-  });
-  return url;
+  let urlParams = (params = new URLSearchParams(params));
+  return "?" + urlParams;
 };
